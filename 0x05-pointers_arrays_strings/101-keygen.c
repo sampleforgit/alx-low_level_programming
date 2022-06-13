6ba66711f94892ab3feb39bdad091afe15b580a0
Mihretu <dmercy2015@gmail.com>
2022-06-13 05:41:12 -0700

    Do not hate the hacker hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - random  numbers
+ *
+ * Return: 0 Always
+*/
+int main(void)
+{
+
+	int i;
+
+	int j = 0;
+
+	int randnum;
+
+	char p[100];
+
+	srand(time(NULL));
+	for (i = 0; i < 100; i++)
+	{
+		randnum = random() % 127;
+		if (randnum > 32)
+		{
+			p[j] = randnum;
+			j++;
+		}
+	}
+	p[j] = '\0';
+	printf("%s", p);
+	return (0);
+}

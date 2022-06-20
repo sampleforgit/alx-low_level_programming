23466776cc210e9763d814cf3765a3e107ff664c
Mihretu <dmercy2015@gmail.com>
2022-06-20 06:06:20 -0700

    100

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,28 +1,36 @@
+#include <stdio.h>
+
 #include "main.h"
 /**
- * rot13 - Function that reverses the content of an array of integers.
- *
- * @str: s is the array
- *
- * Return: Always 0.
+ * *rot13 - encodes to rot13
+ * @s: string to encode
+ * Return: encoded string
  */
-char *rot13(char *str)
+char *rot13(char *s)
 {
-	int i = 0, k;
+	int i, n, r = 13;
+	int array[] = {'a', 'n', 'A', 'N', 'b', 'o', 'B', 'O', 'c', 'p', 'C',
 
-	char s[] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+		'P', 'd', 'q', 'D', 'Q', 'e', 'r', 'E', 'R', 'f', 's', 'F',
 
-	char s1[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+		'S', 'g', 't', 'G', 'T', 'h', 'u', 'H', 'U', 'i', 'v', 'I',
 
-	for (; str[i] != '\0'; i++)
+		'V', 'j', 'w', 'J', 'W', 'k', 'x', 'K', 'X', 'l', 'y', 'L',
+
+		'Y', 'm', 'z', 'M', 'Z'};
+
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		for (k = 0; k <= 51; k++)
+		for (n = 0; n < 52; n++)
 		{
-			if (s[k] == str[i])
+			if (*(s + i) == array[n])
 			{
-				str[i] = s1[k];
+				*(s + i) += r;
+				n = 52;
 			}
+			r *= -1;
 		}
+		r = 13;
 	}
-	return (str);
+	return (s);
 }

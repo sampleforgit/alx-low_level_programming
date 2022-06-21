665436640259e071531f738c9cb5021ef6b1fa7d
Mihretu <dmercy2015@gmail.com>
2022-06-21 07:34:10 -0700

    Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,41 +1,37 @@
+#include <stdio.h>
 #include "main.h"
+int sqr(int, int);
 /**
- * _sqrt_recursion_wrapper - a wrapper that does the recursion bit
- * @n : input number
- * @min: minimum number to guess
- * @max: maximum number to guess
- * Return: square root of @n or -1
-*/
-int _sqrt_recursion_wrapper(int n, int min, int max)
+ * _sqrt_recursion - returns the natural square root of a number
+ *
+ * @n: number to find square root of
+ * Return: natural square root of number, if none return -1
+ */
+int _sqrt_recursion(int n)
 {
-	int guess, guess_squared;
-
-	guess = (min + max) / 2;		/*get guess (g)*/
-	guess_squared = guess * guess;		/*square guess (sg)*/
-	if (guess_squared == n)			/*if squared guess == n return guess*/
-		return (guess);
-	else if (min == max)			/*this means n doesn't have a perfect square*/
+	if (n <= 0)
+	{
 		return (-1);
-	else if (guess_squared < n)		/*overshoot g if sg is less than the n*/
-		return (_sqrt_recursion_wrapper(n, guess + 1, max));
-	else					/*undershoot g if sg is more than the n*/
-		return (_sqrt_recursion_wrapper(n, min, guess - 1));
+	}
+	else
+	{
+		return (sqr(n, 1));
+	}
 }
 /**
- * _sqrt_recursion - a function that returns the natural
- *                   square root of a number
- * @n: input number
- * Return: square root
-*/
-int _sqrt_recursion(int n)
-
+ * sqr - checks square root of a number a
+ * @a: first int that we are checking for square
+ * @b: second int checking for square
+ * Return: int of square root or -1 if none
+ */
+int sqr(int a, int b)
 {
-	if (n == 1)		/*sqrt(1) == 1*/
+	if (a == 1)
 		return (1);
-	else if (n == 0)	/*sqrt(0) == 0*/
-		return (0);
-	else if (n < 0)		/*sqrt(-n) == -1 (NA)*/
+	if (b > a / 2)
 		return (-1);
+	else if (b * b == a)
+		return (b);
 	else
-		return (_sqrt_recursion_wrapper(n, 1, n));
+		return (sqr(a, b + 1));
 }

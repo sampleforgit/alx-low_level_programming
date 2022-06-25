2bed58697400065e3c9168f9d9de77c7790cca2f
Mihretu <dmercy2015@gmail.com>
2022-06-24 18:12:25 -0700

    fourth commit

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -6,4 +6,5 @@ int main(int argc, char *argv[]),
 int main(int __attribute__((unused)) argc, char *argv[]),
 int main(int argc, char __attribute__((unused)) *argv[]),
 int main(int argc, char *argv[]),
+int main(int argc, char *argv[]),
 #endif 

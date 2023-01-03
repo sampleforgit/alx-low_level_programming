18dcbd701247fd8402dec14936dcf3a05c149c7c
Mihretu <dmercy2015@gmail.com>
2023-01-03 14:59:57 -0800

    task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,56 +1,56 @@
 #include "search_algos.h"
 
-void print_array(int *array, size_t i, size_t size);
-
 /**
- * binary_search - binary search algorithm in a string
- * @array: list of elements
- * @size: size of the list
- * @value: target to search
- * Return: element index on success (-1) on fail
+ * print_array - prints the array of ints, separated with commas
+ *
+ * @array: a pointer to the array to print
+ * @first: the first index to print
+ * @last: the last index to print
  */
-int binary_search(int *array, size_t size, int value)
+
+void print_array(int *array, size_t first, size_t last)
 {
-	size_t low = 0, high = size - 1, half = 0;
+	size_t i;
 
-	if (array == NULL || value == 0)
-		return (-1);
-
-	print_array(array, low, size);
-	while (low < high)
+	printf("Searching in array: ");
+	for (i = first; i <= last; i++)
 	{
-		half = (low + high) / 2;
-		if (array[half] < value)
-		{
-			low = half + 1;
-			print_array(array, low, high + 1);
-		}
-		else if (array[half] > value)
-		{
-			high = half - 1;
-			print_array(array, low, high + 1);
-		}
-		else
-			return (array[half]);
+		if (i != first)
+			printf(", ");
+		printf("%d", array[i]);
 	}
-	return (-1);
+	printf("\n");
 }
 
 /**
- * print_array - print all elements in an array
- * @array: list of elements
- * @i: index to init
- * @size: size of the array
+ * binary_search - searches for a value in an array of integers
+ *
+ * @array: a pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value: the value to search for
+ *
+ * Return: the index where value is located or -1 on failure or not found
  */
-void print_array(int *array, size_t i, size_t size)
+
+int binary_search(int *array, size_t size, int value)
 {
-	printf("Searching in array: ");
-	while (i < size)
+	size_t i = 0, first = 0, last = size - 1;
+
+	if (array)
 	{
-		if (i + 1 == size)
-			printf("%d ", array[i]);
-		else
-			printf("%d, ", array[i]);
-		i++;
-	} printf("\n");
+		while (first <= last)
+		{
+			print_array(array, first, last);
+			i = (first + last) / 2; /* Gets the middle value */
+
+			if (value > array[i]) /* Uses the right part of array */
+				first = i + 1;
+			else if (value < array[i]) /* Uses the left part of array */
+				last = i - 1;
+			else
+				return (i);
+		}
+	}
+
+	return (-1);
 }
